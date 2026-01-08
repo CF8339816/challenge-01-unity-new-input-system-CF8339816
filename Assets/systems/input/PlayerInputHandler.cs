7728cb13cefd092c5d8cba4c8f6eda3570c77173
CF8339816 <chris_french_3dga@hotmail.com>
2026-01-08 15:21:38 -0400

    scripting issues
    jump broke when adding  actual movement and context
    
    no movement is working for me debugging and trying to sort it out.

--- Assets/systems/input/PlayerInputHandler.cs
+++ Assets/systems/input/PlayerInputHandler.cs
@@ -1,17 +1,63 @@
+using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerInputScript : MonoBehaviour
+public class PlayerInputHandler : MonoBehaviour
 {
+    
+    
+    [Header("Kick Settings")]
     public Rigidbody sphere;
+    public Vector3 direction = new Vector3(0, 1f, 0.5f); 
+    public float kickforce = 0f;
+    public float minkickforce = 2f;
+    public float maxkickforce = 10f;
+    public float kickforcechargerate = 0.01f;
+    bool charging = false; 
 
-    public void OnKick()
+
+    [Header("Move Values")]
+    public Rigidbody Cube;
+    public Vector2 movement = new Vector2( 1f, 1f);
+    public float movespeed = 2f;
+   
+    [SerializeField] Vector2 moveInput;
+
+
+
+    public void OnKick(/*InputAction.CallbackContext context*/)
     {
-        Debug.Log("kick performed");
+        //if (context.started)
+        //{
+            charging = true;
+            Debug.Log("kick charge started");
+        //}
+
+        //if (context.canceled)
+        //{
+        //    if (kickforce < minkickforce)
+        //    {
+        //        kickforce = minkickforce;
+        //        Debug.Log("kick engaged");
+        //    }
+
+        //    if (kickforce > maxkickforce)
+        //    {
+        //        kickforce = maxkickforce;
+        //        Debug.Log("kick charge completed");
+        //    }
+
+        //}
     }
 
-    public void UpdateUI()
+
+
+    public void OnMove(/*InputAction.CallbackContext context*/)
     {
-        Debug.Log("UIpdate UI called");
+        //moveInput = context.ReadValue<Vector2>;
+        //movement = 
+        Debug.Log("move pls");
+
     }
 
 
