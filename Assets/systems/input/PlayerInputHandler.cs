2d91ac2344bcdc73ab9a9c78fb9d09f1bc23ba14
CF8339816 <chris_french_3dga@hotmail.com>
2026-01-09 00:54:31 -0400

    rebuilding the code again
    Got the ball to move once... just once then the on kick disappeared and would not associate while the context property was actively in the code  doesn't matter because there was no debug log output , tried rebuilding the player input component  then rebuilt the full scene. i am just going to build the code and dop my best to connect it so i have something to review tomorrow as it is 1 am

--- Assets/systems/input/PlayerInputHandler.cs
+++ Assets/systems/input/PlayerInputHandler.cs
@@ -1,120 +1,47 @@
-using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
-   /
 
-    [Header("Kick Settings")]
-    public Rigidbody sphere;
+
+    public Rigidbody sphereRB;
     public Vector3 direction = new Vector3(0, 1f, 0.5f);
-    public float kickforce = 0f;
-    public float minkickforce = 2f;
-    public float maxkickforce = 10f;
-    public float kickforcechargerate = 0.01f;
-    bool charging = false;
+    public float kickforce = 10f;
 
 
-    [Header("Move Values")]
-    public Rigidbody Cube;
-    public Vector2 movement = new Vector2(1f, 1f);
-    public float movespeed = 2f;
-
-    [SerializeField] Vector2 moveInput;
-
-    [Header("Change Dimensions")]
-    public Rigidbody Capsule;
-    public Vector3 scale = new Vector3(1f, 1f, 1f);
-    public float basescale = 1f;
-
-
-
-    
-
-    public void OnKick(/*InputAction.CallbackContext context*/)
+    public void OnKick(InputAction.CallbackContext context)
     {
-        //if (context.started)
-        //{
-        // charging = true;
-        Debug.Log("kick charge started");
-        //}
-
-        //if (context.canceled)
-        //{
-        Debug.Log("kick charge completed");
-        //}
-    }
-
-
-
-    public void OnMove(/*InputAction.CallbackContext context*/)
-    {
+        if (context.started)
         {
-            bool w = Input.GetKey(KeyCode.W);
-            bool s = Input.GetKey(KeyCode.S);
-            bool a = Input.GetKey(KeyCode.A);
-            bool d = Input.GetKey(KeyCode.D);
-            if (w)
-            {
-                Vector3 move = new Vector3(0, 0, 1) * movespeed * Time.deltaTime;
-                Cube.MovePosition(move);
-                Debug.Log("Moved using w key");
+            sphereRB.AddForce(direction * kickforce, ForceMode.Impulse);
 
-                Debug.Log("move pls");
-            }
-            if (s)
-            {
-                Vector3 move = new Vector3(0, 0, -1) * movespeed * Time.deltaTime;
-                Cube.MovePosition(move);
-                Debug.Log("Moved using s key");
+            Debug.Log("Kick Started");
+        }
 
-                Debug.Log("move pls");
-            }
-            if (a)
-            {
-                Vector3 move = new Vector3(-1, 0, 0) * movespeed * Time.deltaTime;
-                Cube.MovePosition(move);
-                Debug.Log("Moved using w key");
+        if (context.canceled)
+        {
+                      
 
-                Debug.Log("move pls");
-            }
-            if (d)
-            {
-                Vector3 move = new Vector3(1, 0, 0) * movespeed * Time.deltaTime;
-                Cube.MovePosition(move);
-                Debug.Log("Moved using w key");
+            Debug.Log("Kick Canceled");
+        }
 
-                Debug.Log("move pls");
-            }
+        else
+        {
+            Debug.Log("Shit done f@c%3d up!");
 
 
         }
     }
 
 
-    public void OnScale(/*InputAction.CallbackContext context*/)
-    {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            //scale = 
 
-            Debug.Log("'Eat Me...'  scale up");
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            Debug.Log("'Drink Me...' scale down");
-        }
-    }
+
+
+
+
 
 }
-
-
-
-
-
-
-
 
 
 
