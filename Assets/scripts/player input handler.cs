90452842af69a21ded0418688dc32536c9b03053
CF8339816 <chris_french_3dga@hotmail.com>
2026-01-12 13:34:08 -0400

    recommit
    it did not commit to classroom on Friday though it told me it did at home.

---
+++ Assets/scripts/player input handler.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class playerinputhandler : MonoBehaviour
+{
+    public Rigidbody sphere;
+
+
+    public void OnKick()
+    {
+        Debug.Log("kick performed");
+
+
+    }
+
+    public void UpdateUI()
+    {
+        Debug.Log("Update UI Called");
+    }
+
+
+
+
+
+}
